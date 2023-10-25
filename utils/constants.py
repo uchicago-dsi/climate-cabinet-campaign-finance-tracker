11b4db2644ae89d3d9731d6273e3e6e53cd0eb14
necabotheking <aichacamara@uchicago.edu>
2023-10-25 12:57:27 -0500

    Create expenditure Constants

--- utils/constants.py
+++ utils/constants.py
@@ -1,6 +1,7 @@
 """
 Constants to be used in various parts of the project
 """
+EXP_FILEPATH = "/Users/necabotheking/Documents/Github/2023-fall-clinic-climate-cabinet/data/Expenditure/"
 
 FILEPATH = "../data/Contributions/"
 
@@ -35,3 +36,55 @@ MI_CONTRIBUTION_COLUMNS = [
     "aggregate",
     "extra_desc",
 ]
+
+# MI_EXPENDITURE_COLUMNS = [
+#     "doc_seq_no", 
+#     "expenditure_type", 
+#     "gub_account_type", 
+#     "gub_elec_type", 
+#     "page_no",	
+#     "expense_id",	
+#     "detail_id",	
+#     "doc_stmnt_year",	
+#     "doc_type_desc",	
+#     "com_legal_name",	
+#     "common_name",	
+#     "cfr_com_id", 
+#     "com_type",	
+#     "schedule_desc", 
+#     "exp_desc",	
+#     "purpose",	
+#     "extra_desc",	
+#     "f_name",	
+#     "lname_or_org",	
+#     "address",	
+#     "city",	
+#     "state",	
+#     "zip",	
+#     "exp_date",	
+#     "amount",	
+#     "state_loc",	
+#     "supp_opp",	
+#     "can_or_ballot", 
+#     "county",	
+#     "debt_payment",	
+#     "vend_name",	
+#     "vend_addr", 
+#     "vend_city",	
+#     "vend_state",	
+#     "vend_zip",	
+#     "gotv_ink_ind",	
+#     "fundraiser"
+# ]
+
+MI_EXPENDITURE_COLUMNS = [ "doc_seq_no", "expenditure_type",
+                          "gub_account_type", "gub_elec_type",
+                          "page_no", "expense_id", "detail_id",
+                          "doc_stmnt_year", "doc_type_desc", "com_legal_name", 
+                          "common_name", "cfr_com_id", "com_type",
+                          "schedule_desc", "exp_desc", "purpose", "extra_desc", 
+                          "f_name", "lname_or_org", "address", "city", "state", 
+                          "zip", "exp_date", "amount", "state_loc", "supp_opp",
+                          "can_or_ballot", "county", "debt_payment", "vend_name",
+                          "vend_addr", "vend_city", "vend_state", "vend_zip",
+                          "gotv_ink_ind", "fundraiser" ]
