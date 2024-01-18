46a5c3b8d297598d561da2aaecc3d90493f8427e
Alan Mburu Kagiri <alankagiri@h001.ds.uchicago.edu>
2024-01-18 00:05:53 -0600

    get_likely function done

--- utils/linkage.py
+++ utils/linkage.py
@@ -3,33 +3,6 @@ Module for performing record linkage on state campaign finance dataset
 """
 
 
-def calculate_string_similarity(string1: str, string2: str) -> float:
-    """Returns how similar two strings are on a scale of 0 to 1
-
-    The exact meaning of the metric is open, but the following must hold true:
-    1. equivalent strings must return 1
-    2. strings with no similar characters must return 0
-    3. strings with higher intuitive similarity must return higher scores
-
-    Args:
-        string1: any string
-        string2: any string
-    Returns:
-        similarity score
-
-    Sample Usage:
-    >>> calculate_string_similarity("exact match", "exact match")
-    1.0
-    >>> calculate_string_similarity("aaaaaa", "bbbbbbbbbbb")
-    0.0
-    >>> similar_score = calculate_string_similarity("very similar", "vary similar")
-    >>> different_score = calculate_string_similarity("very similar", "very not close")
-    >>> similar_socre > different_score
-    True
-    """
-    pass
-
-
 def get_likely_name(first_name: str, last_name: str, full_name: str) -> str:
     """Given name related columns, return a person's likely name
 
@@ -56,54 +29,47 @@ def get_likely_name(first_name: str, last_name: str, full_name: str) -> str:
     >>> get_likely_name("Jane Doe", "Doe", "Jane Doe")
     "Jane Doe"
     """
-    pass
 
+    # if data is clean:
+    if first_name + " " + last_name == full_name:
+        return full_name
 
-def get_address_line_1_from_full_address(address: str) -> str:
-    """Given a full address, return the first line of the formatted address
+    # some names have titles or professions associated with the name. We need to
+    # remove those from the name.
+    titles = [
+        "mr",
+        "ms",
+        "mrs",
+        "miss",
+        "prof",
+        "dr",
+        "doctor",
+        "sir",
+        "madam",
+        "professor",
+    ]
+    names = [first_name, last_name, full_name]
 
-    Address line 1 usually includes street address or PO Box information.
+    for i in range(len(names)):
+        # if there is a ',' switch around the names
+        if "," in names[i]:
+            index = names[i].find(",")
+            first_part = names[i][index + 1 :]
+            last_part = names[i][0:index]
+            names[i] = first_part + " " + last_part
 
-    Args:
-        address: raw string representing full address
-    Returns:
-        address_line_1
+        names[i] = names[i].lower().replace(".", "").split(" ")
+        names[i] = [
+            name_part for name_part in names[i] if name_part not in titles
+        ]
+        names[i] = " ".join(names[i])
 
-    Sample Usage:
-    >>> get_address_line_1_from_full_address("6727 W. Corrine Dr.  Peoria,AZ 85381")
-    "6727 W. Corrine Dr."
-    >>> get_address_line_1_from_full_address("P.O. Box 5456  Sun City West ,AZ 85375")
-    "P.O. Box 5456"
-    >>> get_address_line_1_from_full_address("119 S 5th St  Niles,MI 49120")
-    "119 S 5th St"
-    >>> get_address_line_1_from_full_address(
-    ...     "1415 PARKER STREET APT 251	DETROIT	MI	48214-0000"
-    ... )
-    "1415 PARKER STREET"
-    """
-    pass
-
-
-def get_street_from_address_line_1(address_line_1: str) -> str:
-    """Given an address line 1, return the street name
-
-    Args:
-        address_line_1: either street information or PO box
-    Returns:
-        street name
-    Raises:
-        ValueError: if string is malformed and no street can be reasonably
-            found.
-
-    >>> get_street_from_address_line_1("5645 N. UBER ST")
-    "UBER ST"
-    >>> get_street_from_address_line_1("")
-    Traceback (most recent call last):
-        ...
-    ValueError: address_line_1 must have whitespace
-    >>> get_street_from_address_line_1("PO Box 1111")
-    Traceback (most recent call last):
-        ...
-    ValueError: address_line_1 is PO Box
-    """
-    pass
+    names = " ".join(names)
+    names = names.split(" ")
+    final_name = []
+    [
+        final_name.append(x)
+        for x in names
+        if ((x not in final_name) & (len(x) != 0))
+    ]
+    return " ".join(final_name)
