a51c33777d06cf5aca0097ed456ad5d908771b6d
Nicolas Posner <nicol@MacBook-Pro-5.local>
2024-01-23 20:37:46 -0600

    added explanation of jaro-winkler and reversed strings

--- utils/linkage.py
+++ utils/linkage.py
@@ -8,6 +8,16 @@ Module for performing record linkage on state campaign finance dataset
 def calculate_string_similarity(string1: str, string2: str) -> float:
     """Returns how similar two strings are on a scale of 0 to 1
 
+    This version utilizes Jaro-Winkler distance, which is a metric of 
+    edit distance. Jaro-Winkler specially prioritizes the early 
+    characters in a string. 
+
+    Since the ends of strings are often more valuable in matching names
+    and addresses, we reverse the strings before matching them.
+
+    https://en.wikipedia.org/wiki/Jaro%E2%80%93Winkler_distance
+    https://github.com/Yomguithereal/talisman/blob/master/src/metrics/jaro-winkler.js
+
     The exact meaning of the metric is open, but the following must hold true:
     1. equivalent strings must return 1
     2. strings with no similar characters must return 0
@@ -30,4 +40,4 @@ def calculate_string_similarity(string1: str, string2: str) -> float:
     True
     """
 
-    return float(td.jaro_winkler(string1.lower(), string2.lower()))
+    return float(td.jaro_winkler(string1.lower()[::-1], string2.lower()[::-1]))
