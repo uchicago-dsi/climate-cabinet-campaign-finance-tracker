c8cdcaf10a9121fee065e67b138cf6c923ebb664
Nicolas Posner <nicol@MacBook-Pro-5.local>
2024-01-24 18:20:31 -0600

    added row similarity and row match functions. these do not yet have tests

--- utils/linkage.py
+++ utils/linkage.py
@@ -1,6 +1,8 @@
 """
 Module for performing record linkage on state campaign finance dataset
 """
+import numpy as np
+import pandas as pd
 
 
 def calculate_string_similarity(string1: str, string2: str) -> float:
@@ -107,3 +109,68 @@ def get_street_from_address_line_1(address_line_1: str) -> str:
     ValueError: address_line_1 is PO Box
     """
     pass
+
+
+def calculate_row_similarity(
+    row1: pd.DataFrame, row2: pd.DataFrame, weights: np.array, comparison_func
+) -> float:
+    """Find weighted similarity of two rows in a dataframe
+
+    The length of the weights vector must be the same as
+    the number of selected columns.
+
+    This version is slow and not optimized, and will be
+    revised in order to make it more efficient. It
+    exists as to provide basic functionality. Once we have
+    the comparison function locked in, using .apply will
+    likely be easier and more efficient.
+    """
+
+    row_length = len(weights)
+    if not (row1.shape[1] == row2.shape[1] == row_length):
+        raise ValueError("Number of columns and weights must be the same")
+
+    similarity = np.zeros(row_length)
+
+    for i in range(row_length):
+        similarity[i] = comparison_func(row1.iloc[:, i], row2.iloc[:, i])
+
+    return sum(similarity * weights)
+
+
+def row_matches(df: pd.DataFrame, weights: np.array, threshold: float, comparison_func) -> dict:
+    """Get weighted similarity score of two rows
+
+    Run through the rows using indices: if two rows have a comparison score
+    greater than a threshold, we assign the later row to the former. Any
+    row which is matched to any other row is not examined again. Matches are
+    stored in a dictionary object, with each index appearing no more than once.
+
+    This is not optimized
+    """
+
+    all_indices = np.array(list(df.index))
+
+    index_dict = {}
+    [index_dict.setdefault(x, []) for x in all_indices]
+
+    discard_indices = []
+
+    end = max(all_indices)
+    for i in all_indices:
+        # Skip indices that have been stored in the discard_indices list
+        if i in discard_indices:
+            continue
+
+        # Iterate through the remaining numbers
+        for j in range(i + 1, end):
+            if j in discard_indices:
+                continue
+
+            # Our conditional
+            if calculate_row_similarity(df.iloc[[i]], df.iloc[[j]], weights, comparison_func) > threshold:
+                # Store the other index and mark it for skipping in future iterations
+                discard_indices.append(j)
+                index_dict[i].append[j]
+
+    return index_dict
