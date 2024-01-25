20f4e938e09fa98d1f5acddf7e6eee5c8c2684b5
Adil Kassim <adilk@uchicago.edu>
2024-01-25 05:16:07 +0000

    adding cleaning_company_column function

--- utils/linkage.py
+++ utils/linkage.py
@@ -1,6 +1,7 @@
 """
 Module for performing record linkage on state campaign finance dataset
 """
+import pandas as pd
 import usaddress
 
 
@@ -47,3 +48,54 @@ def get_street_from_address_line_1(address_line_1: str) -> str:
             string.append(key)
 
     return " ".join(string)
+
+
+"""
+Module for standardizing the 'company' columnn of the state campaign finance dataset
+"""
+
+
+def cleaning_company_column(company: str) -> str:
+    """
+    Given a string, check if it contains a variation of self employed, unemployed,
+    or retired and return the standardized version.
+
+    Args:
+        company: string of inputted company names
+    Returns:
+        standardized for retired, self employed, and unemployed,
+        or original string if no match or empty string
+
+    >>> cleaning_company_column("Retireed")
+    'Retired'
+    >>> cleaning_company_column("self")
+    'Self Employed'
+    >>> cleaning_company_column("None")
+    'Unemployed'
+    """
+    if pd.isnull(company):
+        return company
+
+    company_edited = company.lower()
+    company_edited = company_edited.strip()
+    company_edited = company_edited.replace(".", " ")
+    company_edited = company_edited.replace(",", " ")
+    company_edited = company_edited.replace("-", " ")
+
+    if "retire" in company_edited:
+        return "Retired"
+    elif "self employe" in company_edited or company_edited == "self":
+        return "Self Employed"
+    elif (
+        "unemploye" in company_edited
+        or company_edited == "none"
+        or company_edited == "not employed"
+    ):
+        return "Unemployed"
+
+    else:
+        return company
+
+
+# Example implementation of the function standardize_company_column for a dataframe
+# df['standardized_company'] = df['company'].apply(standardize_company_column)
