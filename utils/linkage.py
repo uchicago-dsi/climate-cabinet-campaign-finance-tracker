b5f764a955030fac17adb73bdac9394c2968ed74
Nicolas Posner <nicol@MacBook-Pro-5.local>
2024-01-30 20:40:12 -0600

    addressed comments, but address function back in

--- utils/linkage.py
+++ utils/linkage.py
@@ -1,12 +1,11 @@
 import textdistance as td
-
-# import usaddress
+import numpy as np
+import pandas as pd
+import usaddress
 
 """
 Module for performing record linkage on state campaign finance dataset
 """
-import numpy as np
-import pandas as pd
 
 
 def calculate_string_similarity(string1: str, string2: str) -> float:
@@ -144,3 +143,48 @@ def row_matches(
                 index_dict[i].append[j]
 
     return index_dict
+
+
+def get_street_from_address_line_1(address_line_1: str) -> str:
+    """Given an address line 1, return the street name
+
+    Args:
+        address_line_1: either street information or PO box
+    Returns:
+        street name
+    Raises:
+        ValueError: if string is malformed and no street can be reasonably
+            found.
+
+    >>> get_street_from_address_line_1("5645 N. UBER ST")
+    'UBER ST'
+    >>> get_street_from_address_line_1("")
+    Traceback (most recent call last):
+        ...
+    ValueError: address_line_1 must have whitespace
+    >>> get_street_from_address_line_1("PO Box 1111")
+    Traceback (most recent call last):
+        ...
+    ValueError: address_line_1 is PO Box
+    >>> get_street_from_address_line_1("300 59 St.")
+    '59 St.'
+    >>> get_street_from_address_line_1("Uber St.")
+    'Uber St.'
+    >>> get_street_from_address_line_1("3NW 59th St")
+    '59th St'
+    """
+    if not address_line_1 or address_line_1.isspace():
+        raise ValueError("address_line_1 must have whitespace")
+
+    address_line_lower = address_line_1.lower()
+
+    if "po box" in address_line_lower:
+        raise ValueError("address_line_1 is PO Box")
+
+    string = []
+    address = usaddress.parse(address_line_1)
+    for key, val in address:
+        if val in ["StreetName", "StreetNamePostType"]:
+            string.append(key)
+
+    return " ".join(string)
