cfb6d261f6a2867b6c9521c96d0e7ca8ee4a0693
Alan Mburu Kagiri <alankagiri@g003.ds.uchicago.edu>
2024-02-18 11:27:54 -0600

    testing if path to complete_orgs_table.csv is working

--- tests/test_dedup.py
+++ tests/test_dedup.py
@@ -3,15 +3,15 @@ import pandas as pd
 from utils.constants import BASE_FILEPATH
 from utils.linkage import deduplicate_perfect_matches
 
-inds_sample = pd.read_csv(
-    BASE_FILEPATH / "output" / "complete_individuals_table.csv",
-    low_memory=False,
-)
+# inds_sample = pd.read_csv(
+#    BASE_FILEPATH / "output" / "complete_individuals_table.csv",
+#    low_memory=False,
+# )
 orgs_sample = pd.read_csv(
     BASE_FILEPATH / "output" / "complete_organizations_table.csv"
 )
 
-deduplicated_inds = deduplicate_perfect_matches(inds_sample)
+# deduplicated_inds = deduplicate_perfect_matches(inds_sample)
 deduplicated_orgs = deduplicate_perfect_matches(orgs_sample)
 
 output_dedup_ids = pd.read_csv(
@@ -20,9 +20,9 @@ output_dedup_ids = pd.read_csv(
 # outpud_ids should have all the ids that deduplicated_inds and deduplicated_orgs
 # has
 
-dedup_inds_id = set(deduplicated_inds.id.tolist())
+# dedup_inds_id = set(deduplicated_inds.id.tolist())
 dedup_orgs_id = set(deduplicated_orgs.id.tolist())
 unique_ids = set(output_dedup_ids.duplicated_uuids.tolist())
 
-assert dedup_inds_id.issubset(unique_ids)
+# assert dedup_inds_id.issubset(unique_ids)
 assert dedup_orgs_id.issubset(unique_ids)
