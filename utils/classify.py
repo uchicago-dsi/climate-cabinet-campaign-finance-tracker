ce540957f2083ac4c3db5afd458f4ad8f3391d25
Nicolas Posner <nicol@MacBook-Pro-5.local>
2024-02-18 20:29:34 -0600

    updating classifier

--- utils/classify.py
+++ utils/classify.py
@@ -1,7 +1,5 @@
 import pandas as pd
 
-from utils.linkage import calculate_string_similarity
-
 # we want to run down a list of people and, hopefully, their adresses, plus a list of
 # corporations, groups, etc, and classify them, basically just looking for matches
 
@@ -16,17 +14,62 @@ inds_list = []
 # a list of individual names
 
 
-def similarity_calculator(df: pd.DataFrame, suspect):
-    """Run through a pandas dataframe column and compare elements to a constant"""
-    # this needs to output somehting useful
+def similarity_calculator(
+    df: pd.DataFrame, subject: str, n: int, comparison_func
+) -> pd.DataFrame:
+    """Find best matches to a subject name in a pandas dataframe
 
-    similarities = df["column1"].apply(
-        lambda x: calculate_string_similarity(x, suspect)
+    For a given individual or organization, the subject, we search through the
+    'name'column of a dataframe, select the n highest matches according to a
+    selected comparison function, and return those as a dataframe. This is meant
+    to be used manually to search for matches. For quick automated processing, see
+    automated_classifier().
+
+    Note that the comparison function must take in two inputs, both strings, and
+    output a percentage match
+    """
+
+    similarities_df = df.copy()
+
+    similarities = similarities_df["name"].apply(
+        lambda x: comparison_func(x, subject)
     )
-    return similarities
 
-    # very psuedocode
-    # get top n, maybe just ten, and output
+    similarities_df["similarities"] = similarities
+
+    top_n_matches = similarities_df.sort_values(
+        by=["similarities"], ascending=False
+    )[0:n]
+
+    return top_n_matches
+
+
+def automated_classifier(
+    df: pd.DataFrame, subjects_dict: dict, threshold: float, comparison_func
+):
+    """Using similarity_calculator, classify entities automatically
+
+    Feeding a dictionary of names and the associated statuses, we compare
+    the string matches and, if they exceed a certain threshold, classify
+    them as belonging to some group specified in the subjects dictionary.
+    """
+
+    similarities_df = df.copy()
+
+    for subject in subjects_dict:
+        similarities = similarities_df["name"].apply(
+            lambda x, sub=subject: comparison_func(x, sub)
+            
+        )
+        matches = similarities >= threshold
+
+        status = subjects_dict[subject]
+
+        similarities_df["classification"] = pd.Series(matches).apply(
+            lambda x, stat=status: stat if x else "neutral"
+        )
+
+    return similarities_df
 
     # we can use the indices and/or select manually, just add a new
     # column to the subjects table
