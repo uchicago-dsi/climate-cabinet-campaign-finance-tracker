21af2c951ae837a94a7603af71bdb267349b0f4d
Adil Kassim <adilk@uchicago.edu>
2024-02-22 07:38:04 +0000

    updates

--- utils/linkage_pipeline.py
+++ utils/linkage_pipeline.py
@@ -3,10 +3,13 @@ from typing import Tuple
 import pandas as pd
 from nameparser import HumanName
 
+from utils.constants import BASE_FILEPATH
 from utils.linkage import (
     cleaning_company_column,
+    deduplicate_perfect_matches,
     get_address_line_1_from_full_address,
     get_address_number_from_address_line_1,
+    get_likely_name,
     get_street_from_address_line_1,
     standardize_corp_names,
 )
@@ -32,50 +35,102 @@ def preprocess_pipeline(
     """
     # Preprocess organizations dataframe
     organizations["name"] = (
-        organizations["name"].astype(str, skipna=True).apply(standardize_corp_names)
+        organizations["name"].astype(str).apply(standardize_corp_names)
     )
+    if "Unnamed: 0" in organizations.columns:
+        organizations.drop(columns="Unnamed: 0", inplace=True)
 
     # Preprocess individuals dataframe
     if "Unnamed: 0" in individuals.columns:
         individuals.drop(columns="Unnamed: 0", inplace=True)
 
     individuals = individuals.astype(
-        {"first_name": str, "last_name": str, "full_name": str, "company": str}
+        {
+            "first_name": str,
+            "last_name": str,
+            "full_name": str,
+            "company": "string",
+        }
     )
 
     # Standardize company names in individuals dataframe
-    individuals["company"] = individuals["company"].apply(standardize_corp_names)
-    individuals["company"] = individuals["company"].apply(cleaning_company_column)
+    individuals["company"] = (
+        individuals["company"]
+        .loc[individuals["company"].notnull()]
+        .apply(standardize_corp_names)
+    )
+    individuals["company"] = (
+        individuals["company"]
+        .loc[individuals["company"].notnull()]
+        .apply(cleaning_company_column)
+    )
 
     # Address functions, assuming address column is named 'Address'
-    individuals["Address Line 1"] = individuals["Address"].apply(
-        get_address_line_1_from_full_address
-    )
-    individuals["Street Name"] = individuals["Address Line 1"].apply(
-        get_street_from_address_line_1
-    )
-    individuals["Address Number"] = individuals["Address Line 1"].apply(
-        get_address_number_from_address_line_1
-    )
+    # If there is an "Address" column in the first place
+    if "Address" in individuals.columns:
+        individuals["Address"] = individuals["Address"].astype(str)
+        individuals["Address Line 1"] = individuals["Address"].apply(
+            get_address_line_1_from_full_address
+        )
+        individuals["Street Name"] = individuals["Address Line 1"].apply(
+            get_street_from_address_line_1
+        )
+        individuals["Address Number"] = individuals["Address Line 1"].apply(
+            get_address_number_from_address_line_1
+        )
 
     # Check if first name or last names are empty, if so, extract from full name column
     individuals["full_name"] = individuals["full_name"].astype(str)[
         individuals["full_name"].notnull()
     ]
     if individuals["first_name"].isnull().any():
-        name = individuals["full_name"].apply(HumanName).apply(lambda x: x.as_dict())
+        name = (
+            individuals["full_name"]
+            .apply(HumanName)
+            .apply(lambda x: x.as_dict())
+        )
         first_name = name.apply(lambda x: x["first"])
         individuals["first_name"] = first_name
 
     if individuals["last_name"].isnull().any():
-        name = individuals["full_name"].apply(HumanName).apply(lambda x: x.as_dict())
+        name = (
+            individuals["full_name"]
+            .apply(HumanName)
+            .apply(lambda x: x.as_dict())
+        )
         last_name = name.apply(lambda x: x["last"])
         individuals["last_name"] = last_name
 
-    # Transactions
+    individuals["full_name"] = individuals.apply(
+        lambda row: get_likely_name(
+            row["first_name"], row["last_name"], row["full_name"]
+        ),
+        axis=1,
+    )
+
     if "Unnamed: 0" in transactions.columns:
         transactions.drop(columns="Unnamed: 0", inplace=True)
 
     transactions["purpose"] = transactions["purpose"].str.upper()
 
     return individuals, organizations, transactions
+
+
+organizations = pd.read_csv(
+    BASE_FILEPATH / "output" / "complete_organizations_table.csv"
+)
+
+individuals = pd.read_csv(
+    BASE_FILEPATH / "output" / "complete_individuals_table.csv"
+)
+
+transactions = pd.read_csv(
+    BASE_FILEPATH / "output" / "complete_transactions_table.csv"
+)
+
+individuals, organizations, transactions = preprocess_pipeline(
+    individuals, organizations, transactions
+)
+
+individuals = deduplicate_perfect_matches(individuals)
+organizations = deduplicate_perfect_matches(organizations)
