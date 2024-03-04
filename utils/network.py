96b8e0b33b6d7f9cd9b9ac70d5de1f1ededca7b8
Alan Mburu Kagiri <alankagiri@g002.ds.uchicago.edu>
2024-03-04 12:21:22 -0600

    updated network graph work

--- utils/network.py
+++ utils/network.py
@@ -29,24 +29,77 @@ def name_identifier(uuid: str, dfs: list[pd.DataFrame]) -> str:
     return None
 
 
+def combine_datasets_for_network_graph(dfs: list[pd.DataFrame]) -> pd.DataFrame:
+    """Combines the 3 dataframes into a single dataframe to create the graph
+
+    Given the inds, orgs, and transactions dataframes, the func first finds the
+    recipient_id in the transaction dataframe in either the org or inds
+    dataframes and adds the name of the recipient to the transaction df. Then,
+    the inds and orgs dfs are merged with the transaction df and concatenated
+    with the contributions amount aggregated, making a final dataframe of the
+    merged transactions and entity dataframes.
+
+    Args:
+        list of dataframes in the order: [inds_df, orgs_df, transactions_df]
+        Transactions dataframe with at least column: 'recipient_id'
+        Individuals dataframe with at least column: 'full_name'
+        Organizations dataframe with at least column: 'name'
+
+    Returns
+        A merged dataframe with aggregate contribution amounts between entitites
+    """
+
+    inds_df, orgs_df, transactions_df = dfs
+
+    # first update the transactions df to have a recipient name tied to id
+    transactions_df["recipient_name"] = transactions_df["recipient_id"].apply(
+        name_identifier, args=([orgs_df, inds_df],)
+    )
+
+    # next, merge the inds_df and orgs_df with the transactions_df
+    inds_trans_df = pd.merge(
+        inds_df, transactions_df, how="left", left_on="id", right_on="donor_id"
+    )
+    inds_trans_df = inds_trans_df.dropna(subset=["amount"])
+    orgs_trans_df = pd.merge(
+        orgs_df, transactions_df, how="left", left_on="id", right_on="donor_id"
+    )
+    orgs_trans_df = orgs_trans_df.dropna(subset=["amount"])
+    orgs_trans_df = orgs_trans_df.rename(columns={"name": "full_name"})
+
+    # concatenated the merged dfs
+    merged_df = pd.concat([orgs_trans_df, inds_trans_df])
+
+    # lastly, create the final dataframe with aggregated attributes
+    attribute_cols = merged_df.columns.difference(
+        ["donor_id", "recipient_id", "full_name", "recipient_name"]
+    )
+    agg_functions = {
+        col: "sum" if col == "amount" else "first" for col in attribute_cols
+    }
+    aggreg_df = (
+        merged_df.groupby(
+            ["donor_id", "recipient_id", "full_name", "recipient_name"]
+        )
+        .agg(agg_functions)
+        .reset_index()
+    )
+
+    return aggreg_df
+
+
 def create_network_graph(df: pd.DataFrame) -> nx.MultiDiGraph:
     """Takes in a dataframe and generates a MultiDiGraph where the nodes are
     entity names, and the rest of the dataframe columns make the node attributes
 
     Args:
-        df: a pandas dataframe (complete_individuals_table /
-        complete_organizations_table)
+        df: a pandas dataframe with merged information from the inds, orgs, &
+        transactions dataframes
 
     Returns:
         A Networkx MultiDiGraph with nodes and edges
     """
     G = nx.MultiDiGraph()
-    # first check if df is individuals or organizations dataset
-    if "name" in df.columns:
-        node_name = "name"
-    else:
-        node_name = "full_name"
-
     edge_columns = [
         "office_sought",
         "purpose",
@@ -60,7 +113,7 @@ def create_network_graph(df: pd.DataFrame) -> nx.MultiDiGraph:
     for _, row in df.iterrows():
         # add node attributes based on the columns relevant to the entity
         G.add_node(
-            row[node_name],
+            row["full_name"],
             **row[df.columns.difference(edge_columns)].dropna().to_dict(),
         )
         # add the recipient as a node
@@ -68,7 +121,7 @@ def create_network_graph(df: pd.DataFrame) -> nx.MultiDiGraph:
 
         # add the edge attributes between two nodes
         edge_attributes = row[edge_columns].dropna().to_dict()
-        G.add_edge(row[node_name], row["recipient_name"], **edge_attributes)
+        G.add_edge(row["full_name"], row["recipient_name"], **edge_attributes)
 
     return G
 
@@ -102,11 +155,21 @@ def plot_network_graph(G: nx.MultiDiGraph):
         marker=dict(showscale=True, colorscale="YlGnBu", size=10),
     )
 
+    node_trace["marker"]["color"] = []
     for node in G.nodes():
         node_info = f"Name: {node}<br>"
         for key, value in G.nodes[node].items():
             node_info += f"{key}: {value}<br>"
         node_trace["text"] += tuple([node_info])
+        classification = G.nodes[node].get("classification", "neutral")
+        # Assign a color based on the classification value
+        if classification == "c":
+            color = "blue"
+        elif classification == "f":
+            color = "red"
+        else:
+            color = "green"  # Default color for unknown/neutral classification
+        node_trace["marker"]["color"] += tuple([color])
 
     # Define layout settings
     layout = go.Layout(
