7f3483cf4989799a50e4480d9d21594e0e66facc
Nicolas Posner <nicol@MacBook-Pro-5.local>
2024-03-04 15:09:53 -0600

    updated classify and test_classifier

--- utils/classify.py
+++ utils/classify.py
@@ -3,7 +3,9 @@ import pandas as pd
 from utils.constants import c_org_names, f_companies, f_org_names
 
 
-def classify_wrapper(individuals_df, organizations_df):
+def classify_wrapper(
+    individuals_df: pd.DataFrame, organizations_df: pd.DataFrame
+):
     """Wrapper for classificaiton in linkage pipeline
 
     Initialize the classify column in both dataframes and
@@ -25,7 +27,7 @@ def classify_wrapper(individuals_df, organizations_df):
     return classified_individuals, classified_orgs
 
 
-def matcher(df, substring, column, category):
+def matcher(df: pd.DataFrame, substring: str, column: str, category: str):
     """Applies a label to the classification column based on substrings
 
     We run through a given column containing strings in the dataframe. We
@@ -42,7 +44,7 @@ def matcher(df, substring, column, category):
     return df
 
 
-def classify_individuals(individuals_df):
+def classify_individuals(individuals_df: pd.DataFrame):
     """Part of the classification pipeline
 
     We apply the matcher function to the individuals dataframe
@@ -56,7 +58,7 @@ def classify_individuals(individuals_df):
     return individuals_df
 
 
-def classify_orgs(organizations_df):
+def classify_orgs(organizations_df: pd.DataFrame):
     """Part of the classification pipeline
 
     We apply the matcher function to the organizations dataframe
@@ -71,69 +73,3 @@ def classify_orgs(organizations_df):
         organizations_df = matcher(organizations_df, i, "name", "c")
 
     return organizations_df
-
-
-inds_list = []
-
-# a list of individual names
-
-
-def similarity_calculator(
-    df: pd.DataFrame, subject: str, n: int, comparison_func
-) -> pd.DataFrame:
-    """Find best matches to a subject name in a pandas dataframe
-
-    For a given individual or organization, the subject, we search through the
-    'name'column of a dataframe, select the n highest matches according to a
-    selected comparison function, and return those as a dataframe. This is meant
-    to be used manually to search for matches. For quick automated processing, see
-    automated_classifier().
-
-    Note that the comparison function must take in two inputs, both strings, and
-    output a percentage match
-    """
-
-    similarities_df = df.copy()
-
-    similarities = similarities_df["name"].apply(
-        lambda x: comparison_func(x, subject)
-    )
-
-    similarities_df["similarities"] = similarities
-
-    top_n_matches = similarities_df.sort_values(
-        by=["similarities"], ascending=False
-    )[0:n]
-
-    return top_n_matches
-
-
-def automated_classifier(
-    df: pd.DataFrame, subjects_dict: dict, threshold: float, comparison_func
-):
-    """Using similarity_calculator, classify entities automatically
-
-    Feeding a dictionary of names and the associated statuses, we compare
-    the string matches and, if they exceed a certain threshold, classify
-    them as belonging to some group specified in the subjects dictionary.
-    """
-
-    similarities_df = df.copy()
-
-    for subject in subjects_dict:
-        similarities = similarities_df["name"].apply(
-            lambda x, sub=subject: comparison_func(x, sub)
-        )
-        matches = similarities >= threshold
-
-        status = subjects_dict[subject]
-
-        similarities_df["classification"] = pd.Series(matches).apply(
-            lambda x, stat=status: stat if x else "neutral"
-        )
-
-    return similarities_df
-
-    # we can use the indices and/or select manually, just add a new
-    # column to the subjects table
-    # that marks fossil fuels, green energy, or neither
