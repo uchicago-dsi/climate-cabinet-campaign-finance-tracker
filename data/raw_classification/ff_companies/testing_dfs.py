5f2af254c211570a606742d5ed3387ae03ef8ed9
klee2024 <88099550+klee2024@users.noreply.github.com>
2024-04-05 15:53:32 -0500

    Delete data/raw_classification/ff_companies/testing_dfs.py

--- data/raw_classification/ff_companies/testing_dfs.py
+++
@@ -1,7 +0,0 @@
-# pylint: disable=all
-import pandas as pd
-
-coal_companies_df = pd.read_csv("FFF_coal_companies.csv")
-oil_companies_df = pd.read_csv("FFF_oil_companies.csv")
-print(coal_companies_df.head())
-print(oil_companies_df.head())
