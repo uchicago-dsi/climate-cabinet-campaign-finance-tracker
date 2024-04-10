bde1c06ed9bf1bb6745087192d1c562dd5d1ba4f
Kaya Lee <klee2020@pingry.org>
2024-04-10 21:53:23 +0000

    reorganized get_ff_companies.py into pipeline to combine known company data from FFF and InfoGroup dataset

--- EDA/uchicago_business_EDA.py
+++ EDA/uchicago_business_EDA.py
@@ -3,8 +3,33 @@
 import pandas as pd
 from utils.constants import BASE_FILEPATH
 
+# PATHS
 business_data_2023 = (
     BASE_FILEPATH / "data" / "raw_classification" / "2023_Business_Academic_QCQ.txt"
+)
+
+clean_energy_csv = (
+    BASE_FILEPATH
+    / "data"
+    / "raw_classification"
+    / "infogroup_biz_data"
+    / "infogroup_CE.csv"
+)
+
+ff_csv = (
+    BASE_FILEPATH
+    / "data"
+    / "raw_classification"
+    / "infogroup_biz_data"
+    / "infogroup_ff.csv"
+)
+
+ambiguous_csv = (
+    BASE_FILEPATH
+    / "data"
+    / "raw_classification"
+    / "infogroup_biz_data"
+    / "infogroup_ambiguous.csv"
 )
 
 cols = [
@@ -16,6 +41,7 @@ cols = [
     "AREA CODE",
     "PRIMARY SIC CODE",
     "SIC6_DESCRIPTIONS (SIC)",
+    "initial classification",
 ]
 
 ff_keywords = ["OIL", "GAS", "FOSSIL", "FUEL", "COAL", "GASOLINE"]
@@ -26,11 +52,13 @@ ff_keywords_df = pd.DataFrame()
 ce_keywords_df = pd.DataFrame()
 ambiguous_keywords_df = pd.DataFrame()
 
+# allows us to see if any of the keywords are present in the SIC code description
 pattern_ff = "|".join(ff_keywords)
 pattern_ce = "|".join(ce_keywords)
 pattern_ambg = "|".join(ambiguous_keywords)
 
 business_data_df = pd.read_csv(business_data_2023, sep=",", header=0, chunksize=1000)
+counter = 0  # only looking at around 20 chunks for now since file is quite large
 for chunk in business_data_df:
     chunk = chunk.dropna(subset=["SIC6_DESCRIPTIONS (SIC)"])
     ff_keywords_df = pd.concat(
@@ -51,7 +79,18 @@ for chunk in business_data_df:
             chunk[chunk["SIC6_DESCRIPTIONS (SIC)"].str.contains(pattern_ambg)],
         ],
     )
+    counter += 1
+    if counter >= 20:
+        break
 
-print(ff_keywords_df["SIC6_DESCRIPTIONS (SIC)"].unique())
-print(ce_keywords_df["SIC6_DESCRIPTIONS (SIC)"].unique())
-print(ambiguous_keywords_df["SIC6_DESCRIPTIONS (SIC)"].unique())
+ce_keywords_df["initial classification"] = "c"
+ff_keywords_df["initial classification"] = "f"
+ambiguous_keywords_df["initial classification"] = None
+
+ce_keywords_df[cols].to_csv(clean_energy_csv, mode="w", index=False)
+ff_keywords_df[cols].to_csv(ff_csv, mode="w", index=False)
+ambiguous_keywords_df[cols].to_csv(ambiguous_csv, mode="w", index=False)
+
+print(pd.read_csv(clean_energy_csv).head())
+print(pd.read_csv(ff_csv).head())
+print(pd.read_csv(ambiguous_csv).head())
---
+++ src/utils/orgs_classification_data_pipeline.py
@@ -0,0 +1,76 @@
+"""Script to combine datasets of fossil fule company names from CSVs for use in classification"""
+
+import pandas as pd
+
+from utils.constants import BASE_FILEPATH
+
+# PATHS
+
+coal_company_csv = (
+    BASE_FILEPATH
+    / "data"
+    / "raw_classification"
+    / "ff_companies"
+    / "FFF_coal_companies.csv"
+)
+oil_company_csv = (
+    BASE_FILEPATH
+    / "data"
+    / "raw_classification"
+    / "ff_companies"
+    / "FFF_oil_companies.csv"
+)
+
+infogroup_clean_energy_csv = (
+    BASE_FILEPATH
+    / "data"
+    / "raw_classification"
+    / "infogroup_biz_data"
+    / "infogroup_CE.csv"
+)
+
+infogroup_ff_csv = (
+    BASE_FILEPATH
+    / "data"
+    / "raw_classification"
+    / "infogroup_biz_data"
+    / "infogroup_ff.csv"
+)
+
+infogroup_ambiguous_csv = (
+    BASE_FILEPATH
+    / "data"
+    / "raw_classification"
+    / "infogroup_biz_data"
+    / "infogroup_ambiguous.csv"
+)
+
+
+organization_classification_csv = (
+    BASE_FILEPATH / "data" / "raw_classification" / "organization_classifications.csv"
+)
+
+
+# CLEANING & OUTPUT
+
+# disclaimer: companies are global (perhaps not all US companies )
+ff_companies_dfs = [pd.read_csv(coal_company_csv), pd.read_csv(oil_company_csv)]
+ff_companies_dfs = pd.concat(ff_companies_dfs)
+
+# adding a classification column
+ff_companies_dfs["initial classification"] = ["f"] * len(ff_companies_dfs)
+
+infogroup_CSVs = [infogroup_clean_energy_csv, infogroup_ambiguous_csv, infogroup_ff_csv]
+infogroup_dfs = [pd.read_csv(csv) for csv in infogroup_CSVs]
+# renaming COMPANY column to Company to merge
+infogroup_dfs = [
+    df.rename(mapper={"COMPANY": "Company"}, axis=1) for df in infogroup_dfs
+]
+concatted_infogroups_dfs = pd.concat(infogroup_dfs)
+
+# combining all the datasets into one to be written to csv file
+all_ff_companies = pd.concat([ff_companies_dfs, concatted_infogroups_dfs])
+
+
+# writes the df to the organization_classification csv file
+all_ff_companies.to_csv(organization_classification_csv, mode="w", index=False)
