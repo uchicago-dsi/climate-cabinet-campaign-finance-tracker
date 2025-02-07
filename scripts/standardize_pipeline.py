4d98382bbfbb802aa2cae89f83027bd5b95329ce
Trevor Spreadbury <trevorspreadbury@gmail.com>
2025-02-07 13:06:32 -0600

    create standardization pipeline

---
+++ scripts/standardize_pipeline.py
@@ -0,0 +1,41 @@
+"""Script for running cleaning pipeline"""
+
+import argparse
+
+from utils.constants import BASE_FILEPATH
+from utils.finance.pipeline import standardize_states
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "-s",
+    "--states",
+    default=None,
+    help="State abbreviations of all states to run pipeline on",
+)
+parser.add_argument(
+    "-o",
+    "--output-directory",
+    default=None,
+    help="Path to directory to save output. Default is 'output/transformed'",
+)
+args = parser.parse_args()
+
+if args.output_directory is None:
+    output_directory = BASE_FILEPATH / "output" / "transformed"
+else:
+    output_directory = args.output_directory
+if args.states is None:
+    states = None
+else:
+    states = args.states
+output_directory.mkdir(parents=True, exist_ok=True)
+
+individuals_output_path = output_directory / "individuals_table-*.csv"
+organizations_output_path = output_directory / "organizations_table-*.csv"
+transactions_output_path = output_directory / "transactions_table-*.csv"
+id_table_output_path = output_directory / "id_map-*.csv"
+database = standardize_states(states=states)
+for table_type in database:
+    database[table_type].to_csv(output_directory / f"{table_type}.csv")
+print("pipeline finished and save data to csv.")
