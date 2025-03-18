f46f9264418b033e9897048358d1f6dc368ce0a8
Trevor Spreadbury <trevorspreadbury@gmail.com>
2025-03-18 08:59:46 -0500

    move static config out of python files

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -92,41 +92,6 @@ organizations_settings = {
 
 organizations_blocking = ["l.name = r.name"]
 
-# individuals compnay f names
-f_companies = [
-    "exxon",
-    "chevron",
-    "southwest gas",
-    "petroleum",
-    "koch industries",
-    "koch companies",
-    "oil & gas",
-    "marathon oil",
-    "shell oil",
-]
-
-# organizations f names
-f_org_names = [
-    "koch industries",
-    "koch pac",
-    "kochpac",
-    "southwest gas az",
-    "pinnacle west",
-    "americans for prosperity",
-    "energy transfer",
-]
-
-# organizations c names
-c_org_names = [
-    "clean energy",
-    "vote solar action",
-    "renewable",
-    "pattern energy",
-    "beyond carbon",
-    "lcv victory",
-    "league of conservation",
-]
-
 suffixes = [
     "sr",
     "jr",
