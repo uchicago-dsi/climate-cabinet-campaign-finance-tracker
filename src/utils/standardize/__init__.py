f0cc77c5ea4e1288205ee4a633ca6ed1f6fe0feb
Trevor Spreadbury <trevorspreadbury@gmail.com>
2025-09-18 18:49:33 +0000

    update standardize package docstring

--- src/utils/standardize/__init__.py
+++ src/utils/standardize/__init__.py
@@ -1,7 +1,7 @@
 """Package for reading and standardizing state level campaign data
 
 The DataSource class is subclassed for each unique source of data. For
-more information, see TODO and CONTRIBUTING.md for how to add additional
+more information, see CONTRIBUTING.md for how to add additional
 states
 """
 
