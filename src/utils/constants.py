8aa33469050a24159ef40e202995b0a49f566c6a
Trevor Spreadbury <trevorspreadbury@gmail.com>
2025-09-22 09:14:40 -0500

    add code for setting DATA_DIR

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,10 +1,15 @@
 """Constants to be used in various parts of the project."""
 
+import os
 from pathlib import Path
 
+import dotenv
+
+dotenv.load_dotenv()
 BASE_FILEPATH = Path(__file__).resolve().parent.parent.parent
-DATA_DIR = BASE_FILEPATH / "data"
-RAW_DATA_DIRECTORY = BASE_FILEPATH / "data" / "raw"
+DATA_DIR = os.getenv("DATA_DIR", BASE_FILEPATH / "data")
+DATA_DIR = Path(DATA_DIR).resolve()
+RAW_DATA_DIRECTORY = DATA_DIR / "raw"
 STATE_FINANCE_CONFIG_DIRECTORY = (
     BASE_FILEPATH / "src" / "utils" / "standardize" / "finance" / "config"
 )
